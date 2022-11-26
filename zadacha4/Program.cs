6a3a320ab706adc54fd975bf457cdbc46d103e2c
«Saro» <opac4321@mail.ru>
2022-11-26 15:57:12 +0300

    закончили 4 задачу

---
+++ zadacha4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+
+Console.WriteLine(" Ведите число: ");
+
+int number = int.Parse(Console.ReadLine());
+
+int evennum = 2; //первое четное число
+
+if(number < 2 )
+
+Console.WriteLine(" четных чисел нет");
+Console.WriteLine( evennum  );
+
+while(evennum  < number )
+     {evennum = evennum + 2;
+     Console.WriteLine( evennum  );
+     };
+  
